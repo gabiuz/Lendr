b01da761152f8498bea5f0b44cb284ba899127f5
gabiuz <pelagionicolai@gmail.com>
2025-12-03 14:26:32 +0800

    add categories icon

---
+++ client/app/components/Categories.jsx
@@ -0,0 +1,14 @@
+export default function Categories(){
+
+  const svg = [
+    {link: ` /Users/gab/Downloads/categories/WIREFRAME 5/loveseat-duotone-light-full 1.svg /Users/gab/Downloads/categories/WIREFRAME 4/screwdriver-wrench-duotone-light-full (1) 1.svg /Users/gab/Downloads/categories/WIREFRAME 3/clothes-hanger-duotone-light-full 1.svg /Users/gab/Downloads/categories/WIREFRAME 2/computer-speaker-duotone-light-full 1.svg /Users/gab/Downloads/categories/WIREFRAME/Group 4.svg`}
+  ]
+
+  return (
+    <>
+      <div>
+
+      </div>
+    </>
+  )
+}
