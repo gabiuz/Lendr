5e0db500295f802cbdc209a184170822cd21fb42
Gabiuz <pelagionicolai@gmail.com>
2025-12-05 21:41:45 +0800

    removed opacity on images

--- client/app/register/page.jsx
+++ client/app/register/page.jsx
@@ -18,128 +18,141 @@ export default function Register() {
       { label: "Prefer Not to Say", id: "other", value: "other" },
     ],
     emailAndPass: [
-      { label: "Email", id: "email", type: "email", placeholder: "Abcde@gmail.com" },
-      { label: "Password", id: "password", type: "password", placeholder: "*********" },
+      {
+        label: "Email",
+        id: "email",
+        type: "email",
+        placeholder: "Abcde@gmail.com",
+      },
+      {
+        label: "Password",
+        id: "password",
+        type: "password",
+        placeholder: "*********",
+      },
     ],
     addressAndPhone: [
-      { label: "Address", id: "address", type: "text", placeholder: "123 st. Metro Manila" },
-      { label: "Phone Number", id: "contactNo", type: "tel", placeholder: "+63" },
-    ]
+      {
+        label: "Address",
+        id: "address",
+        type: "text",
+        placeholder: "123 st. Metro Manila",
+      },
+      {
+        label: "Phone Number",
+        id: "contactNo",
+        type: "tel",
+        placeholder: "+63",
+      },
+    ],
   };
 
   return (
-    <>
-      <div className="relative min-h-screen">
-        <div className="absolute inset-0 bg-[url('/bg-image-register.jpg')] bg-cover bg-center opacity-55"></div>
-        <div className="absolute z-10 flex justify-center w-full top-6 md:w-full md:justify-center lg:w-auto lg:justify-start lg:left-24 lg:top-16">
-          <Image
-            src="/lendr-logo.png"
-            alt="Lendr Logo"
-            width={142}
-            height={54}
-          />
-        </div>
-        <div className="z-10 relative flex justify-center items-end lg:justify-end min-h-screen">
-          <Form className="register w-full lg:w-5xl">
-            <div className="bg-white px-6 py-10 lg:px-22 lg:py-20 lg:mr-28">
-              <div className="red-line mx-auto mb-8"></div>
-              <div className="flex flex-col justify-center items-center lg:mb-8 lg:items-start">
-                <h1 className="text-2xl font-bold text-black mt-0 mb-1 w-fit text-center lg:text-5xl">
-                  Create an Account<span className="m-0 p-0 text-red">.</span>
-                </h1>
-                <p className="text-sm text-black mb-6 lg:text-base">
-                  Already have an Account?
-                  <Link href="/login" className="font-bold">
-                    <span> Log in</span>
-                  </Link>
-                </p>
-              </div>
-              <div className="Name-container flex flex-col gap-4 mb-4 lg:flex-row ">
-                {formFields.name.map((field) => (
-                  <Input
-                    key={field.id}
-                    label={field.label}
-                    type="text"
-                    placeholder={field.placeholder}
-                    name={field.id}
-                    id={field.id}
-                    required={true}
-                    containerClassName="w-full lg:w-[235px]"
-                  ></Input>
-                ))}
-              </div>
-              <div className="bday-radio-container flex flex-col gap-4 mb-4 lg:flex-row">
-                <div className="radio-container w-full lg:mr-8">
-                  <label className="text-black mb-1.5 block">
-                    Sex<span className="text-red">*</span>
-                  </label>
-                  <div className="flex w-full gap-3 justify-between lg:w-fit">
-                    {formFields.sex.map((field) => (
-                      <RadioButtons
-                        key={field.id}
-                        id={field.id}
-                        name={field.id}
-                        value={field.value}
-                        label={field.label}
-                      ></RadioButtons>
-                    ))}
-                  </div>
-                </div>
-                <Input
-                  label="Birthday"
-                  type="date"
-                  name="bday"
-                  id="bday"
-                  placeholder="Birthday"
-                  className="text-light-gray"
-                  containerClassName="w-full"
-                  required={true}
-                ></Input>
-              </div>
-              <div className="email-password flex flex-col  gap-4 mb-4 lg:flex-row">
-                {formFields.emailAndPass.map((field) => (
-                  <Input
-                    key={field.id}
-                    label={field.label}
-                    htmlFor={field.id}
-                    type={field.type}
-                    name={field.id}
-                    id={field.id}
-                    required={true}
-                    placeholder={field.placeholder}
-                    containerClassName="w-full"
-                  ></Input>
-                ))}
-              </div>
-              <div className="address-phone flex flex-col  gap-4 mb-4 lg:flex-row">
-                {formFields.addressAndPhone.map((field) => (
-                  <Input
-                    key={field.id}
-                    label={field.label}
-                    htmlFor={field.id}
-                    type={field.type}
-                    name={field.id}
-                    id={field.id}
-                    required={true}
-                    placeholder={field.placeholder}
-                    containerClassName="w-full"
-                  ></Input>
-                ))}
-              </div>
-              <div className="button-container flex flex-row justify-between gap-3 lg:justify-end">
-                <Button
-                  label="Cancel"
-                  className="text-light-gray flex-1 lg:flex-0 hover:text-red hover:border-red"
-                ></Button>
-                <Button
-                  label="Submit"
-                  className="text-white bg-light-gray flex-1 lg:flex-0 hover:bg-red hover:border-red"
-                ></Button>
-              </div>
-            </div>
-          </Form>
-        </div>
+    <div className="min-h-screen bg-[url('/bg-image-register.jpg')] bg-cover bg-center">
+      <div className="flex justify-center w-full pt-6 md:justify-center lg:justify-start lg:pl-24 lg:pt-16">
+        <Image src="/lendr-logo.png" alt="Lendr Logo" width={142} height={54} />
       </div>
-    </>
+
+      <div className="flex justify-center items-end lg:justify-end h-screen">
+        <Form className="register w-full lg:w-5xl">
+          <div className="bg-white px-6 py-10 lg:px-22 lg:py-20 lg:mr-28">
+            <div className="red-line mx-auto mb-8"></div>
+            <div className="flex flex-col justify-center items-center lg:mb-8 lg:items-start">
+              <h1 className="text-2xl font-bold text-black mt-0 mb-1 w-fit text-center lg:text-5xl">
+                Create an Account<span className="m-0 p-0 text-red">.</span>
+              </h1>
+              <p className="text-sm text-black mb-6 lg:text-base">
+                Already have an Account?
+                <Link href="/login" className="font-bold">
+                  <span> Log in</span>
+                </Link>
+              </p>
+            </div>
+            <div className="Name-container flex flex-col gap-4 mb-4 lg:flex-row">
+              {formFields.name.map((field) => (
+                <Input
+                  key={field.id}
+                  label={field.label}
+                  type="text"
+                  placeholder={field.placeholder}
+                  name={field.id}
+                  id={field.id}
+                  required={true}
+                  containerClassName="w-full lg:w-[235px]"
+                />
+              ))}
+            </div>
+            <div className="bday-radio-container flex flex-col gap-4 mb-4 lg:flex-row">
+              <div className="radio-container w-full lg:mr-8">
+                <label className="text-black mb-1.5 block">
+                  Sex<span className="text-red">*</span>
+                </label>
+                <div className="flex w-full gap-3 justify-between lg:w-fit">
+                  {formFields.sex.map((field) => (
+                    <RadioButtons
+                      key={field.id}
+                      id={field.id}
+                      name={field.id}
+                      value={field.value}
+                      label={field.label}
+                    />
+                  ))}
+                </div>
+              </div>
+              <Input
+                label="Birthday"
+                type="date"
+                name="bday"
+                id="bday"
+                placeholder="Birthday"
+                className="text-light-gray"
+                containerClassName="w-full"
+                required={true}
+              />
+            </div>
+            <div className="email-password flex flex-col gap-4 mb-4 lg:flex-row">
+              {formFields.emailAndPass.map((field) => (
+                <Input
+                  key={field.id}
+                  label={field.label}
+                  htmlFor={field.id}
+                  type={field.type}
+                  name={field.id}
+                  id={field.id}
+                  required={true}
+                  placeholder={field.placeholder}
+                  containerClassName="w-full"
+                />
+              ))}
+            </div>
+            <div className="address-phone flex flex-col gap-4 mb-4 lg:flex-row">
+              {formFields.addressAndPhone.map((field) => (
+                <Input
+                  key={field.id}
+                  label={field.label}
+                  htmlFor={field.id}
+                  type={field.type}
+                  name={field.id}
+                  id={field.id}
+                  required={true}
+                  placeholder={field.placeholder}
+                  containerClassName="w-full"
+                />
+              ))}
+            </div>
+            <div className="button-container flex flex-row justify-between gap-3 lg:justify-end">
+              <Button
+                label="Cancel"
+                className="text-light-gray flex-1 lg:flex-0 hover:text-red hover:border-red"
+              />
+              <Button
+                label="Submit"
+                className="text-white bg-light-gray flex-1 lg:flex-0 hover:bg-red hover:border-red"
+              />
+            </div>
+          </div>
+        </Form>
+      </div>
+    </div>
   );
 }
