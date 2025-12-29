d2cb73f9910117d49d456d4bb7a78ca01172e347
gabiuz <pelagionicolai@gmail.com>
2025-12-29 18:29:38 +0800

    fixed search component on homepage

--- client/app/homepage/page.jsx
+++ client/app/homepage/page.jsx
@@ -99,13 +99,13 @@ export default function Homepage() {
 
   return (
     <div className="min-h-screen w-full bg-white">
-      <Navbar className="sticky" />
+      <Navbar />
 
       <header
-        className="h-[500px] md:h-[600px] xl:h-[750px] bg-[url('/homepage-bg-image.jpg')] bg-opacity bg-cover bg-center"
+        className="min-h-[650px] md:min-h-[600px] xl:min-h-[750px] bg-[url('/homepage-bg-image.jpg')] bg-opacity bg-cover bg-center"
         id="home"
       >
-        <div className="flex flex-col items-center gap-4 md:gap-6 pt-32 md:pt-40 text-center px-4">
+        <div className="flex flex-col items-center gap-4 md:gap-6 pt-24 md:pt-40 text-center px-4">
           <h1 className="text-white font-bold text-3xl md:text-4xl xl:text-6xl">
             Rent Anything, <span className="text-red"> Anytime</span>
           </h1>
@@ -114,24 +114,9 @@ export default function Homepage() {
             check availability, and rent securely — all in one place.
           </p>
         </div>
-        <div className="flex justify-center items-center px-4 py-4">
-          <div className="flex bg-white lg:px-4 lg:py-4 lg:w-fit lg:gap-5 items-center rounded-2xl lg:mt-16">
+        <div className="flex justify-center items-center px-4 py-6 md:py-4">
+          <div className="flex bg-white px-4 py-4 lg:px-4 lg:py-4 lg:w-fit lg:gap-5 items-center rounded-2xl lg:mt-16">
             <Search />
-            <button className="cursor-pointer">
-              <svg
-                width="66"
-                height="66"
-                viewBox="0 0 66 66"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <rect width="66" height="66" rx="33" fill="#FF0000" />
-                <path
-                  d="M40.3594 30.1092C40.3594 23.9357 35.3605 18.9354 29.1886 18.9354C23.0168 18.9354 18.0179 23.9357 18.0179 30.1092C18.0179 36.2827 23.0168 41.283 29.1886 41.283C35.3605 41.283 40.3594 36.2827 40.3594 30.1092ZM38.2021 41.4995C35.7305 43.4619 32.5957 44.6351 29.1886 44.6351C21.1666 44.6351 14.6666 38.1334 14.6666 30.1092C14.6666 22.0851 21.1666 15.5833 29.1886 15.5833C37.2107 15.5833 43.7107 22.0851 43.7107 30.1092C43.7107 33.5172 42.5377 36.6529 40.5759 39.1251L49.9244 48.4761C49.9244 48.4761 50.5807 49.1326 50.5807 50.1941C50.5807 51.2556 49.9244 51.4931 49.2681 51.4931C48.6118 51.4931 48.2069 51.5 47.5576 50.8436L38.2021 41.4995Z"
-                  fill="white"
-                />
-              </svg>
-            </button>
           </div>
         </div>
       </header>
